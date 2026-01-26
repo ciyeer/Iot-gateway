a0cc28e54477720c5ea53c73a656728f5075ce6f
ciyeer <ciyeer@outlook.com>
2026-01-27 00:22:37 +0800

    style: 统一时间工具函数中的缩进格式
    将函数内的代码缩进统一为4个空格，以保持代码风格的一致性并提高可读性。

--- IotEdgeGateway/src/core/common/utils/time_utils.hpp
+++ IotEdgeGateway/src/core/common/utils/time_utils.hpp
@@ -14,29 +14,29 @@ namespace common {
 namespace time {
 
 inline std::int64_t NowUnixMs() {
-  const auto now = std::chrono::system_clock::now();
-  const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
-  return static_cast<std::int64_t>(ms.count());
+    const auto now = std::chrono::system_clock::now();
+    const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch());
+    return static_cast<std::int64_t>(ms.count());
 }
 
 inline std::string NowIso8601Utc() {
-  const auto now = std::chrono::system_clock::now();
-  const auto tt = std::chrono::system_clock::to_time_t(now);
+    const auto now = std::chrono::system_clock::now();
+    const auto tt = std::chrono::system_clock::to_time_t(now);
 
-  std::tm tm{};
+    std::tm tm{};
 #if defined(_WIN32)
-  gmtime_s(&tm, &tt);
+    gmtime_s(&tm, &tt);
 #else
-  gmtime_r(&tt, &tm);
+    gmtime_r(&tt, &tm);
 #endif
 
-  std::ostringstream oss;
-  oss << std::put_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
-  return oss.str();
+    std::ostringstream oss;
+    oss << std::put_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
+    return oss.str();
 }
 
 inline void SleepMs(std::uint32_t ms) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
 
 }  // namespace time
